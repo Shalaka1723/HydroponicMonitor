0d213e150f6364cac85c975f5e6643cd19780585
Shalaka <shalakaw17@gmail.com>
2024-06-20 21:58:32 +0530

    respo

--- hydroponicMonitor/src/App.jsx
+++ hydroponicMonitor/src/App.jsx
@@ -13,12 +13,9 @@ const App = () => {
 
   return (
     <>
-      <div className="flex flex-col bg-lime-100 m-[1%] place-items-center h-screen rounded-lg">
-        <div className=" bg-sky-900 top-1/2 shadow-lg fixed rounded-full w-32 h-32 px-5 text-center text-white z-10 opacity-90">
-          <h1 className=" mt-5 text-wrap text-base">MY HYDROPONIC STATS</h1>
-        </div>
+      <div className="flex flex-col bg-lime-200 m-[1%] place-items-center h-screen rounded-lg">
         <div className="flex flex-row h-1/2 w-full m-4 justify-center">
-          <div className=" bg-lime-300 opacity-75 rounded-xl w-1/3 m-4">
+          <div className=" bg-white opacity-75 rounded-xl w-1/3 m-4">
             <h1 className=" text-center text-sky-900 font-semibold text-3xl mt-2">
               Water Temperature
             </h1>
@@ -36,8 +33,8 @@ const App = () => {
               />
             </div>
           </div>
-          <div className=" bg-sky-700 shadow-lg rounded-full w-96 h-96">
-            <div className=" grid grid-cols-2 m-2 pl-5 pt-20 text-white text-center items-center justify-items-center  ">
+          <div className=" bg-sky-700 flex relative shadow-lg rounded-full w-96 h-96 justify-items-end justify-center items-center ">
+            <div className=" grid grid-cols-2 m-2 pl-5  text-white text-center items-center justify-items-center  ">
               <div className=" ">
                 <div className="my-3   ">Water Ph</div>
                 <div className="my-3  ">Water Temperature</div>
@@ -45,7 +42,6 @@ const App = () => {
                 <div className="my-3  ">Humidy</div>
                 <div className="my-3  ">Water Level</div>
               </div>
-
               <div className="  ">
                 <div className="my-3  ">7</div>
                 <div className="my-3  ">25°C</div>
@@ -54,9 +50,12 @@ const App = () => {
                 <div className="my-3  ">30%</div>
               </div>
             </div>
+                <div className=" bg-sky-900 shadow-lg absolute top-[78%] rounded-full w-32 h-32 px-5 text-center text-white z-10 opacity-90">
+                  <h1 className=" mt-5 text-wrap text-base">MY HYDROPONIC STATS</h1>
+                </div>
           </div>
-          <div className=" bg-lime-300 opacity-75 rounded-xl w-1/3 m-4">
-            <h1 className=" text-center text-sky-900 font-semibold text-3xl mt-2">
+          <div className=" bg-white opacity-75 rounded-xl w-1/3 m-4">
+            <h1 className=" text-center text-lime-500 font-semibold text-3xl mt-2">
               Humidity
             </h1>
             <Gauge
@@ -76,8 +75,8 @@ const App = () => {
           </div>
         </div>
         <div className="flex flex-row h-1/2 w-full m-4 justify-center">
-          <div className=" bg-lime-300 opacity-75 rounded-xl w-1/3 m-4">
-            <h1 className=" text-center text-sky-900 font-semibold text-3xl mt-2">
+          <div className=" bg-white opacity-75 rounded-xl w-1/3 m-4">
+            <h1 className=" text-center text-lime-500 font-semibold text-3xl mt-2">
               Water ph
             </h1>
             <h1 className=" text-xl text-center shadow-md content-center"></h1>
@@ -122,8 +121,8 @@ const App = () => {
               />
             </div>
           </div>
-          <div className=" bg-lime-300 opacity-75 rounded-3xl w-1/3 m-4">
-            <h1 className=" text-center text-sky-900 font-semibold text-3xl mt-2">
+          <div className=" bg-white opacity-75 rounded-3xl w-1/3 m-4">
+            <h1 className=" text-center text-lime-500 font-semibold text-3xl mt-2">
               Water Level
             </h1>
             <Gauge
@@ -141,7 +140,7 @@ const App = () => {
               text={({ value }) => `${value}%`}
             />
           </div>
-          <div className=" bg-lime-300 opacity-75 rounded-xl w-1/3 m-4">
+          <div className=" bg-white opacity-75 rounded-xl w-1/3 m-4">
             <h1 className=" text-center text-sky-900 font-semibold text-3xl mt-2">
               Atmospheric Temperature
             </h1>
